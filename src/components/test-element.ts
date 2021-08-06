72b1d41d2295dabb1521de28239f4a001a1d62fd
Dominique Rochefort <dominique.rochefort@hmhco.com>
2021-08-06 09:17:11 -0400

    simplify

--- src/components/test-element.ts
+++ src/components/test-element.ts
@@ -1,18 +1,15 @@
-import { LitElement, html, TemplateResult, render } from "lit";
-import { customElement, property } from "lit/decorators.js";
+import { LitElement, html, TemplateResult } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
 
-@customElement("test-element")
+@customElement('test-element')
 export class TestElement extends LitElement {
-  @property() bar: string;
+    @property() bar: string;
 
-  protected render(): TemplateResult {
-    render(html`<div class="foo">${this.bar}</div>`, this);
+    protected render(): TemplateResult {
+        return html`<div class="foo">${this.bar}</div>`;
+    }
 
-    return html`<h3>In the shadow DOM</h3>
-      <slot></slot>`;
-  }
-
-  public alterTemplateProgrammatically(content: string): void {
-    this.querySelector(".foo").innerHTML = content;
-  }
+    public alterTemplateProgrammatically(content: string): void {
+        this.shadowRoot.querySelector('.foo').innerHTML = content;
+    }
 }
--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,5 @@
-import { TestElement } from "./components/test-element";
-const element = document.querySelector<TestElement>("test-element");
+import { TestElement } from './components/test-element';
+const element = document.querySelector<TestElement>('test-element');
 
-element.alterTemplateProgrammatically("foo");
-element.bar = "bar";
+element.alterTemplateProgrammatically('foo');
+element.bar = 'bar';
